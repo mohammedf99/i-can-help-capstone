14e6a16910427ee51d3592bb7d4b0fd507dda2b2
Omerrj <67536912+Omerrj@users.noreply.github.com>
2021-07-06 16:55:10 +0300

    add signup , setUser functions

--- src/Utilities/FirebaseUtilities.js
+++ src/Utilities/FirebaseUtilities.js
@@ -1,3 +1,38 @@
+/* eslint-disable import/prefer-default-export */
+
 import Firebase from "../Firebase";
 
 // Add the functions used in firebase here.
+const usersRef = Firebase.firestore().collection("users");
+// adds user with info to users collection
+
+export const setUser = ({ email, name }, id) => {
+  usersRef.doc(id).set({
+    contact: { email },
+    name,
+    description: null,
+    job: null,
+    gender: null,
+    langSelected: 1,
+    location: null,
+    picture: null,
+    languages: [],
+    posts: [],
+    pinnedPosts: [],
+  });
+};
+
+export const signUp = ({ email, password, name }) => {
+  try {
+    Firebase.auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(({ user }) =>
+        user.sendEmailVerification({
+          url: "https://i-can-help-20773.web.app",
+        })
+      )
+      .then(() => setUser({ email, name }, Firebase.auth().currentUser.uid));
+  } catch (error) {
+    alert(error.message);
+  }
+};
