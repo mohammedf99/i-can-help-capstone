c221f3fd97b3cc74225a1d89a8f91fb4e728f294
mohammedf99 <mohammed.hamidf99@gmail.com>
2021-07-06 13:14:08 +0300

    create left side of the footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,26 @@
 import React from "react";
+import {
+  FooterSty,
+  LeftSideSty,
+  LogoSty,
+  FooterParaSty,
+  SMWrapperSty,
+  SMIconSty,
+} from "./Footer.styled";
 
-const Footer = () => {
-
-
-}
+const Footer = () => (
+  <FooterSty>
+    <LeftSideSty>
+      <LogoSty>I can help</LogoSty>
+      <FooterParaSty>Copyright &copy; 2021 ICT team.</FooterParaSty>
+      <FooterParaSty>All rights reserved.</FooterParaSty>
+      <SMWrapperSty>
+        <SMIconSty>GH</SMIconSty>
+        <SMIconSty>FB</SMIconSty>
+        <SMIconSty>YT</SMIconSty>
+      </SMWrapperSty>
+    </LeftSideSty>
+  </FooterSty>
+);
 
 export default Footer;
---
+++ src/components/Footer/Footer.styled.jsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+
+export const FooterSty = styled.footer`
+  background-color: #1c1256;
+  color: #fafaff;
+  width: 100%;
+  height: 360px;
+  padding: 64px 165px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const LeftSideSty = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const LogoSty = styled.h1`
+  color: #fafaff;
+  font-size: 32px;
+  font-weight: 600;
+  margin-bottom: 2rem;
+`;
+
+export const FooterParaSty = styled.p`
+  font-size: 16px;
+  margin-bottom: 0.5rem;
+`;
+
+export const SMWrapperSty = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-top: 2rem;
+`;
+
+export const SMIconSty = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 40px;
+  height: 40px;
+  background-color: rgba(250, 250, 255, 0.1);
+  border-radius: 50%;
+  margin-right: 0.75rem;
+`;
