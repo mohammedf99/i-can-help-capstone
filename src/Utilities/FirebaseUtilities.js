f142f04fd3404d16e1024342f31bf5b3417a2c74
Omerrj <67536912+Omerrj@users.noreply.github.com>
2021-07-06 17:10:33 +0300

    add userUpdate and post functions

--- src/Utilities/FirebaseUtilities.js
+++ src/Utilities/FirebaseUtilities.js
@@ -4,6 +4,7 @@ import Firebase from "../Firebase";
 
 // Add the functions used in firebase here.
 const usersRef = Firebase.firestore().collection("users");
+const postsRef = Firebase.firestore().collection("posts");
 // adds user with info to users collection
 
 export const setUser = ({ email, name }, id) => {
@@ -22,6 +23,16 @@ export const setUser = ({ email, name }, id) => {
   });
 };
 
+export const updateUser = (values, id) => {
+  try {
+    usersRef.doc(id).update({
+      ...values,
+    });
+  } catch (error) {
+    alert(error);
+  }
+};
+
 export const signUp = ({ email, password, name }) => {
   try {
     Firebase.auth()
@@ -34,5 +45,26 @@ export const signUp = ({ email, password, name }) => {
       .then(() => setUser({ email, name }, Firebase.auth().currentUser.uid));
   } catch (error) {
     alert(error.message);
+  }
+};
+
+export const post = (values, user) => {
+  try {
+    postsRef.add({
+      createdDate: values.createdDate,
+      description: values.description,
+      employment: values.employment,
+      gender: user.gender,
+      location: user.location,
+      pictures: values.pictures,
+      price: values.price,
+      time: values.time,
+      title: values.title,
+      type: values.type,
+      user: user.id,
+      views: 0,
+    });
+  } catch (error) {
+    alert(error);
   }
 };
