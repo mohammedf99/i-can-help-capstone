b120ec06191c505c46e9030baa66afc0dd71b74f
mohammedf99 <mohammed.hamidf99@gmail.com>
2021-07-06 13:15:05 +0300

    import footer to see the result of my code

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import Navbar from "../components/Navbar/Navbar";
+import Footer from "@components/Footer/Footer";
 // import { Navbar } from "@components/Navbar/Navbar";
 
 function Homepage() {
@@ -12,6 +13,7 @@ function Homepage() {
     <div>
       {t("title")}
       <Navbar />
+      <Footer />
     </div>
   );
 }
