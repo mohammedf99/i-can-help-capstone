d13df61ebbf4011b07cc38d1da43ded8471e254e
mohammedf99 <mohammed.hamidf99@gmail.com>
2021-07-07 14:02:31 +0300

    finish left side of the footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,32 @@
 import React from "react";
-import {FooterSty} from "./Footer.styled";
+import { Row, Col } from "antd";
+import { FooterSty, LogoSty, FooterParaSty, SMWrapperSty, SMIconSty } from "./Footer.styled";
 
 const Footer = () => (
   <FooterSty>
-
+    <Row>
+      <Col span={12}>
+        <Row>
+          <Col span={24}>
+            <LogoSty>I can help</LogoSty>
+          </Col>
+          <Col span={24}>
+            <FooterParaSty>Copyright &copy; 2021 ICH team.</FooterParaSty>
+            <FooterParaSty>All rights reserved.</FooterParaSty>
+          </Col>
+          <Col span={24}>
+            <SMWrapperSty>
+              <SMIconSty />
+              <SMIconSty />
+              <SMIconSty />
+            </SMWrapperSty>
+          </Col>
+        </Row>
+      </Col>
+      <Col span={12}>
+        <h1>right side</h1>
+      </Col>
+    </Row>
   </FooterSty>
 );
 
