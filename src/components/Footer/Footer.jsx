9e9438789b49194b155258a5763c1821379bde4c
mohammedf99 <mohammed.hamidf99@gmail.com>
2021-07-07 14:07:07 +0300

    copied new footer component branch to fontawesome

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,63 +1,32 @@
 import React from "react";
-import {
-  FooterSty,
-  LeftSideSty,
-  LogoSty,
-  FooterParaSty,
-  SMWrapperSty,
-  SMIconSty,
-  RightSideSty,
-  RightSectSty,
-  FooterH2Sty,
-  Ul,
-  Li,
-  ContactDivSty,
-  InputDivSty,
-  InputSty,
-  InputIconSty,
-} from "./Footer.styled";
+import { Row, Col } from "antd";
+import { FooterSty, LogoSty, FooterParaSty, SMWrapperSty, SMIconSty } from "./Footer.styled";
 
 const Footer = () => (
   <FooterSty>
-    <LeftSideSty>
-      <LogoSty>I can help</LogoSty>
-      <FooterParaSty>Copyright &copy; 2021 ICT team.</FooterParaSty>
-      <FooterParaSty>All rights reserved.</FooterParaSty>
-      <SMWrapperSty>
-        <SMIconSty>GH</SMIconSty>
-        <SMIconSty>FB</SMIconSty>
-        <SMIconSty>YT</SMIconSty>
-      </SMWrapperSty>
-    </LeftSideSty>
-    <RightSideSty>
-      <RightSectSty>
-        <FooterH2Sty>Company</FooterH2Sty>
-        <Ul>
-          <Li>About us</Li>
-          <Li>Blog</Li>
-          <Li>Contact us</Li>
-          <Li>Testimonials</Li>
-        </Ul>
-      </RightSectSty>
-      <RightSectSty>
-        <FooterH2Sty>Support</FooterH2Sty>
-        <Ul>
-          <Li>Help center</Li>
-          <Li>Terms of service</Li>
-          <Li>Legal</Li>
-          <Li>Privacy policy</Li>
-        </Ul>
-      </RightSectSty>
-      <ContactDivSty>
-        <FooterH2Sty>Contact Us</FooterH2Sty>
-        <form>
-          <InputDivSty>
-            <InputSty placeholder="Enter your email" />
-            <InputIconSty>S</InputIconSty>
-          </InputDivSty>
-        </form>
-      </ContactDivSty>
-    </RightSideSty>
+    <Row>
+      <Col span={12}>
+        <Row>
+          <Col span={24}>
+            <LogoSty>I can help</LogoSty>
+          </Col>
+          <Col span={24}>
+            <FooterParaSty>Copyright &copy; 2021 ICH team.</FooterParaSty>
+            <FooterParaSty>All rights reserved.</FooterParaSty>
+          </Col>
+          <Col span={24}>
+            <SMWrapperSty>
+              <SMIconSty />
+              <SMIconSty />
+              <SMIconSty />
+            </SMWrapperSty>
+          </Col>
+        </Row>
+      </Col>
+      <Col span={12}>
+        <h1>right side</h1>
+      </Col>
+    </Row>
   </FooterSty>
 );
 
