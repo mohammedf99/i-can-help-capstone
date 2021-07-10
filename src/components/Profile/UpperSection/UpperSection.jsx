a248b4fdf2e5b037fa5dce7723d2ae90f81baab1
mohammedf99 <mohammed.hamidf99@gmail.com>
2021-07-10 14:04:33 +0300

    fix eslint format

--- src/components/Profile/UpperSection/UpperSection.jsx
+++ src/components/Profile/UpperSection/UpperSection.jsx
@@ -2,7 +2,15 @@ import React from "react";
 import { Row, Col } from "antd";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
-import { ProImgDiv, ProUserName, WorkTitle, LocationText, MyPostsDiv, MyPinsDiv, numOfPosts } from "./UpperSection.styled";
+import {
+  ProImgDiv,
+  ProUserName,
+  WorkTitle,
+  LocationText,
+  MyPostsDiv,
+  MyPinsDiv,
+  numOfPosts,
+} from "./UpperSection.styled";
 
 const UpperSection = () => (
   <section>
@@ -32,17 +40,19 @@ const UpperSection = () => (
       </Col>
       <Col span={16}>
         <Row>
-            <Col span={8}>
-                <MyPostsDiv>
-                  <div className="postTitle">
-                    <h3>My posts</h3>
-                  </div>
-                  <span className="numOfPosts">20</span>
-                </MyPostsDiv>
-            </Col>
-            <Col span={8}>
-                <MyPinsDiv><h1>Pinned posts</h1></MyPinsDiv>
-            </Col>
+          <Col span={8}>
+            <MyPostsDiv>
+              <div className="postTitle">
+                <h3>My posts</h3>
+              </div>
+              <span className="numOfPosts">20</span>
+            </MyPostsDiv>
+          </Col>
+          <Col span={8}>
+            <MyPinsDiv>
+              <h1>Pinned posts</h1>
+            </MyPinsDiv>
+          </Col>
         </Row>
       </Col>
     </Row>
