60b196218cf7ea5aa1dd69e4350bff22ef807d26
User <emanhharki@gmail.com>
2021-07-14 20:31:42 +0300

    Category

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,9 +4,7 @@ import { Header } from "./Navbar.styled";
 
 const Navbar = () => (
   <div>
-    {/* // <p>This is an example of a component</p>
-    // <Button type="primary">Example of Antd button</Button>
-    // <Header>WELCOME</Header> */}
+
   </div>
 );
 
