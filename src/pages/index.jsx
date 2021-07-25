bb46db2ed42f4d3df2322f6952eeed3f26aa5ee3
mohammedf99 <mohammed.hamidf99@gmail.com>
2021-07-25 12:24:34 +0300

    to view the component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import Navbar from "../components/Navbar/Navbar";
+import Post from "../components/Post/Post";
 // import { Navbar } from "@components/Navbar/Navbar";
 
 function Homepage() {
@@ -12,6 +13,7 @@ function Homepage() {
     <div>
       {t("title")}
       <Navbar />
+      <Post />
     </div>
   );
 }
