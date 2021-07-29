89bec955f60dc68115e2045f3f8401f15de4d347
Omerrj <67536912+Omerrj@users.noreply.github.com>
2021-07-29 18:33:22 +0300

    add image perspective

--- src/components/HomeHero/HomeHero.jsx
+++ src/components/HomeHero/HomeHero.jsx
@@ -1,17 +1,33 @@
-import { Row, Typography, Col } from "antd";
-import { ImageStyled } from "./HomeHero.styled";
+import { Row, Col } from "antd";
+import {
+  ImageStyled,
+  TitleStyled,
+  DescriptionStyled,
+  ImageWrapper,
+  SecondaryButton,
+  PrimaryButton,
+} from "./HomeHero.styled";
 
 const HomeHero = () => (
-  <Row style={{ margin: "10px" }}>
-    <Col span={8}>
-      <Typography.Title>Hire the best workers in Kurdistan </Typography.Title>
-      <Typography.Paragraph>
-        Find great talent. Build your business. Take your career to the next
-        level.
-      </Typography.Paragraph>
+  <Row style={{ margin: "40px" }}>
+    <Col span={10}>
+      <TitleStyled>Hire the best workers in Kurdistan </TitleStyled>
+
+      <DescriptionStyled>
+        Find great talent. Build your business.
+      </DescriptionStyled>
+
+      <DescriptionStyled>Take your career to the next level.</DescriptionStyled>
+
+      <Row>
+        <SecondaryButton>Hire a talent</SecondaryButton>
+        <PrimaryButton>Find a Job</PrimaryButton>
+      </Row>
     </Col>
-    <Col span={16}>
-      <ImageStyled src="https://images.unsplash.com/photo-1513692398020-cbaea622c427?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" />
+    <Col span={14}>
+      <ImageWrapper>
+        <ImageStyled src="https://images.unsplash.com/photo-1513692398020-cbaea622c427?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" />
+      </ImageWrapper>
     </Col>
   </Row>
 );
