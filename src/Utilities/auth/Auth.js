9a93c3fcd4c6f6bb4d692fa9fb86571b8b81a839
Omerrj <67536912+Omerrj@users.noreply.github.com>
2021-08-01 18:23:04 +0300

    add authProvider to hold user state
    add LoggedIn component to protect children component

---
+++ src/Utilities/auth/Auth.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState, createContext, useContext } from "react";
+import { useRouter } from "next/router";
+import Firebase from "../../Firebase";
+import { getUser } from "../FirebaseUtilities";
+
+export const AuthContext = createContext({ currentUser: false, loading: true });
+
+export const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [userData, setUserData] = useState({});
+
+  const authStateChange = (user) => {
+    if (user) getUser(user.uid).then(setUserData);
+    else setUserData({});
+    setCurrentUser(user);
+    setLoading(!loading);
+  };
+
+  useEffect(() => {
+    const unsubscribe = Firebase.auth().onAuthStateChanged(authStateChange);
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ currentUser, loading, userData }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+const LoggedIn = ({ children }) => {
+  const { currentUser, loading } = useContext(AuthContext);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!loading && !currentUser) router.push("/");
+  }, [currentUser, loading]);
+
+  return <>{currentUser && children}</>;
+};
+
+export default LoggedIn;
