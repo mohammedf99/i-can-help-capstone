1840b061d7188212a8775dcedbd6f3795bbadca7
Omerrj <67536912+Omerrj@users.noreply.github.com>
2021-08-01 18:20:45 +0300

    fix signup would signin automaticaly fix error catching add getUser function to get user data

--- src/Utilities/FirebaseUtilities.js
+++ src/Utilities/FirebaseUtilities.js
@@ -5,29 +5,35 @@ import Firebase from "../Firebase";
 // Add the functions used in firebase here.
 const usersRef = Firebase.firestore().collection("users");
 const postsRef = Firebase.firestore().collection("posts");
+
 // adds user with info to users collection
 
-export const setUser = ({ email, name }, id) => {
-  usersRef.doc(id).set({
-    contact: { email },
-    name,
-    description: null,
-    job: null,
-    gender: null,
-    langSelected: 1,
-    location: null,
-    picture: null,
-    languages: [],
-    posts: [],
-    pinnedPosts: [],
-  });
-};
+export const setUser = ({ email, name }, id) =>
+  usersRef
+    .doc(id)
+    .set({
+      contact: { email },
+      name,
+      description: null,
+      job: null,
+      gender: null,
+      langSelected: 1,
+      location: null,
+      picture: null,
+      languages: [],
+      posts: [],
+      pinnedPosts: [],
+    })
+    .catch((e) => console.log(e));
 
 export const updateUser = (values, id) => {
   try {
-    usersRef.doc(id).update({
-      ...values,
-    });
+    usersRef
+      .doc(id)
+      .update({
+        ...values,
+      })
+      .catch((e) => alert(e));
   } catch (error) {
     alert(error);
   }
@@ -37,16 +43,22 @@ export const signUp = ({ email, password, name }, callback) => {
   try {
     Firebase.auth()
       .createUserWithEmailAndPassword(email, password)
-      .then(({ user }) =>
+
+      .then(({ user }) => {
         user.sendEmailVerification({
           url: "https://i-can-help-20773.web.app",
-        })
-      )
+        });
+
+        setUser({ email, name }, user.uid).then(() =>
+          Firebase.auth().signOut()
+        );
+      })
+
       .then(() => {
         alert("signup success");
-        setUser({ email, name }, Firebase.auth().currentUser.uid);
-        callback();
-      });
+        if (callback) callback();
+      })
+      .catch((e) => alert(e));
   } catch (error) {
     alert(error);
   }
@@ -59,7 +71,7 @@ export const signIn = ({ email, password }, callback) => {
       .then((r) => {
         if (!r.user.emailVerified) return alert("please verify to log in");
         alert("signin success");
-        callback();
+        if (callback) callback();
       })
       .catch((e) => alert(e.message));
   } catch (error) {
@@ -69,20 +81,22 @@ export const signIn = ({ email, password }, callback) => {
 
 export const post = (values, user) => {
   try {
-    postsRef.add({
-      createdDate: values.createdDate,
-      description: values.description,
-      employment: values.employment,
-      gender: user.gender,
-      location: user.location,
-      pictures: values.pictures,
-      price: values.price,
-      time: values.time,
-      title: values.title,
-      type: values.type,
-      user: user.id,
-      views: 0,
-    });
+    postsRef
+      .add({
+        createdDate: values.createdDate,
+        description: values.description,
+        employment: values.employment,
+        gender: user.gender,
+        location: user.location,
+        pictures: values.pictures,
+        price: values.price,
+        time: values.time,
+        title: values.title,
+        type: values.type,
+        user: user.id,
+        views: 0,
+      })
+      .catch((e) => alert(e));
   } catch (error) {
     alert(error);
   }
@@ -97,4 +111,14 @@ export const signout = (callback) => {
   } catch (e) {
     alert(e.message);
   }
+};
+
+export const getUser = async (id) => {
+  const data = await usersRef
+    .doc(id)
+    .get()
+    .then((snapshot) => snapshot.data())
+    .catch((e) => alert(e.message));
+
+  return data;
 };
