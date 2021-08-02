11c820827df221fb482bf1af9bd8d273934a1553
mohammedf99 <74563035+mohammedf99@users.noreply.github.com>
2021-08-02 20:15:38 +0300

    profile upper section structure

--- src/components/Profile/UpperSection/UpperSection.jsx
+++ src/components/Profile/UpperSection/UpperSection.jsx
@@ -2,67 +2,51 @@ import React from "react";
 import { Row, Col } from "antd";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMapMarkerAlt, faUser } from "@fortawesome/free-solid-svg-icons";
-import {
-  ProImgDiv,
-  ProUserName,
-  WorkTitle,
-  LocationText,
-  MyPostsDiv,
-  PinnedPostsDiv,
-} from "./UpperSection.styled";
+import { UpperSectionCont, MyPostsCard, PinnedPostCard } from "./UpperSection.styled";
 
 const UpperSection = () => (
-  <section>
-    <Row>
-      <Col span={8}>
-        <Row>
-          <Col span={8}>
-            <ProImgDiv>
-              <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="profile" />
-            </ProImgDiv>
-          </Col>
-          <Col span={16}>
-            <Row>
-              <Col span={24}>
-                <ProUserName>John Doe</ProUserName>
-              </Col>
-              <Col span={24}>
-                <WorkTitle>Gardner</WorkTitle>
-              </Col>
-              <Col span={24}>
-                <FontAwesomeIcon icon={faMapMarkerAlt} style={{ color: "#EE4266" }} />
-                <LocationText>Erbil, IQ</LocationText>
-              </Col>
-            </Row>
-          </Col>
-        </Row>
-      </Col>
-      <Col span={16}>
-        <Row>
-          <Col span={8}>
-            <MyPostsDiv className="post-card">
-              <div className="card-upper">
-                <h2>5</h2>
-              </div>
-              <div className="card-lowe">
-                <h3><a href="#">My posts</a></h3>
-              </div>
-            </MyPostsDiv>
-          </Col>
-          <Col span={8}>
-            <PinnedPostsDiv className="card">
-              <div className="card-upper">
-                <h2>5</h2>
-              </div>
-              <div className="card-lower">
-                <h3><a href="#">Pinned posts</a></h3>
-              </div>
-            </PinnedPostsDiv>
-          </Col>
-        </Row>
-      </Col>
-    </Row>
-  </section>
+  <UpperSectionCont>
+    <div className="left-section">
+      <div className="img-div">
+        <img
+          src="https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-4-300x300.png"
+          alt="user image"
+        />
+      </div>
+      <div className="user-info-div">
+        <h1 className="name">John doe</h1>
+        <h2 className="job">Gardner</h2>
+        <h3 className="location">
+          <span className="location-icon">
+            <FontAwesomeIcon icon={faMapMarkerAlt} />
+          </span>
+          Sulaymaniyah
+        </h3>
+      </div>
+    </div>
+    <div className="right-section">
+      <MyPostsCard>
+        <div className="card-upper">
+          <h3 className="post-number">5</h3>
+        </div>
+        <div className="card-lower">
+          <h3 className="post-title">
+            <a href="#">My posts</a>
+          </h3>
+        </div>
+      </MyPostsCard>
+      <PinnedPostCard>
+        <div className="card-upper">
+          <h3 className="post-number">5</h3>
+        </div>
+        <div className="card-lower">
+          <h3 className="post-title">
+            <a href="#">Pinned posts</a>
+          </h3>
+        </div>
+      </PinnedPostCard>
+    </div>
+  </UpperSectionCont>
 );
 
 export default UpperSection;
