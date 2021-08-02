17b3aed455445b7e4ebed1852847f6ad37bac334
mohammedf99 <74563035+mohammedf99@users.noreply.github.com>
2021-08-02 20:25:51 +0300

    my posts page structure

--- src/pages/myPosts/myPostsPage.jsx
+++ src/pages/myPosts/myPostsPage.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import UpperSection from "../../components/Profile/UpperSection/UpperSection";
+import ProfilePost from "../../components/ProfilePost/ProfilePost";
+import { ProfileSection, PostsContainer } from "./myPostsPage.styled";
 
 function MyPostsPage() {
-  return <h1>My posts</h1>;
+  return (
+    <>
+      <ProfileSection>
+        <UpperSection />
+      </ProfileSection>
+
+      <PostsContainer>
+        <h3 className="my-post-title">My posts</h3>
+        <div className="posts">
+          <ProfilePost />
+          <ProfilePost />
+          <ProfilePost />
+          <ProfilePost />
+          <ProfilePost />
+          <ProfilePost />
+          <ProfilePost />
+          <ProfilePost />
+          <ProfilePost />
+        </div>
+      </PostsContainer>
+    </>
+  );
 }
 
 export default MyPostsPage;
