916899fe23b4eec3d07ad0986076da27f8c4c61e
mohammedf99 <74563035+mohammedf99@users.noreply.github.com>
2021-08-02 22:04:30 +0300

    add name, salary, and delete button to the component

--- src/components/ProfilePost/ProfilePost.jsx
+++ src/components/ProfilePost/ProfilePost.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { PostCardCont } from "./ProfilePost.styled";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import { PostCardCont, PostCardBody } from "./ProfilePost.styled";
 
 const ProfilePost = () => {
   const { Meta } = PostCardCont;
@@ -10,10 +12,30 @@ const ProfilePost = () => {
       style={{ width: 240 }}
       cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
     >
-      <Meta description="12 hours ago" />
-      <a href="#">
-        <p>Hidden universe revealed in stunning first images from German telescope</p>
-      </a>
+      <PostCardBody>
+        <div className="upper-body">
+          <h4 className="name">John Doe</h4>
+          <h4 className="salary">$5/day</h4>
+        </div>
+        <div className="middle-body">
+          <a href="#">
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet tenetur dicta nobis
+              ullam quisquam eius dolorem nesciunt minima error maxime natus, voluptate nihil
+              delectus corporis fugiat id cupiditate iure in aliquid ut voluptatem non ex eligendi.
+              Cum expedita dolorum accusantium beatae dolores laborum aliquid veritatis quod odit,
+              consequuntur velit nesciunt.
+            </p>
+          </a>
+        </div>
+        <div className="lower-body">
+          <form action="">
+            <button type="submit">
+              <FontAwesomeIcon icon={faTrashAlt} />
+            </button>
+          </form>
+        </div>
+      </PostCardBody>
     </PostCardCont>
   );
 };
