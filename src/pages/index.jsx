ad052bbe694254aac96accfd02c4e7eac6938eb7
mohammedf99 <74563035+mohammedf99@users.noreply.github.com>
2021-08-03 09:14:45 +0300

    Profile post component (#43)
    * Profile post component
    
    * to view the component result
    
    * add name, salary, and delete button to the component
    
    * add style to new added name, salary, and delete button
    
    * pass the test
    
    * add margin bottom

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import Testimonials from "../components/Testimonials/Testimonials";
-import Footer from "../components/Footer/Footer";
-import NavbarSection from "../components/Navbar/NavbarSection";
+
+import Navbar from "../components/Navbar/Navbar";
 import ProfilePost from "../components/ProfilePost/ProfilePost";
-import UpperSection from "../components/Profile/UpperSection/UpperSection";
-import Category from '../components/Category/Category';
+// import { Navbar } from "@components/Navbar/Navbar";
 
 function Homepage() {
   // This controls the direction of the page for RTL languages
@@ -14,11 +12,9 @@ function Homepage() {
 
   return (
     <div>
-      <NavbarSection />
-      <Testimonials />
-      <UpperSection />
-      <Footer />
-      <Category />
+      {t("title")}
+      <Navbar />
+      <ProfilePost />
     </div>
   );
 }
