6878f4fa554aca30e10dc990f872a01a50be9763
Omerrj <67536912+Omerrj@users.noreply.github.com>
2021-08-03 22:23:40 +0300

    add authenticated navbar with some other fixes

--- src/components/Navbar/SignedinNavbar.jsx
+++ src/components/Navbar/SignedinNavbar.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Avatar } from "antd";
 
 import { createFromIconfontCN } from "@ant-design/icons";
+import { signout } from "../../Utilities/FirebaseUtilities";
 
 const IconFont = createFromIconfontCN({
   scriptUrl: "//at.alicdn.com/t/font_8d5l8fzk5b87iudi.js",
@@ -21,6 +22,7 @@ const SignedinNavbar = () => (
       size={35}
       icon={<IconFont type="icon-tuichu" size="21.25px" />}
       style={{ backgroundColor: "#EE4266", marginLeft: "15px" }}
+      onClick={() => signout()}
     />
   </div>
 );
--- src/components/Navbar/SignedoutNavbar.jsx
+++ src/components/Navbar/SignedoutNavbar.jsx
@@ -4,13 +4,14 @@ import { Button } from "antd";
 
 import { DownOutlined, UserOutlined } from "@ant-design/icons";
 
-const SignedoutNavbar = () => (
+const SignedoutNavbar = ({ auth }) => (
   <div className="btn">
     <Button
       className="btn1"
       type="button"
       shape="circle"
       icon={<UserOutlined />}
+      onClick={() => auth.changeSigninVisible()}
     />
 
     <Button
@@ -18,6 +19,7 @@ const SignedoutNavbar = () => (
       type="button"
       shape="circle"
       icon={<DownOutlined className="arrow" />}
+      onClick={() => auth.changeSignupVisible()}
     />
   </div>
 );
