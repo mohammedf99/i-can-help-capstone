9aaf04f530fba276eee9b7f749fb62c281259c49
Omerrj <67536912+Omerrj@users.noreply.github.com>
2021-08-03 17:07:38 +0300

    Navbar fix (#44)
    * fix navbar change with authentication
    
    * add antd icons dependency

--- src/components/Navbar/SignedinNavbar.jsx
+++ src/components/Navbar/SignedinNavbar.jsx
@@ -1,12 +1,8 @@
 import React from "react";
 
-import { Button, Avatar } from "antd";
+import { Avatar } from "antd";
 
-import {
-  DownOutlined,
-  UserOutlined,
-  createFromIconfontCN,
-} from "@ant-design/icons";
+import { createFromIconfontCN } from "@ant-design/icons";
 
 const IconFont = createFromIconfontCN({
   scriptUrl: "//at.alicdn.com/t/font_8d5l8fzk5b87iudi.js",
@@ -14,20 +10,17 @@ const IconFont = createFromIconfontCN({
 
 const SignedinNavbar = () => (
   <div className="btn">
-    <Avatar className="avatar"
+    <Avatar
+      className="avatar"
       src="https://images.squarespace-cdn.com/content/v1/5fff77cecbbc994cdc1ee140/1610738319344-3G1CDOQBSD6BF3F5U3JD/billy-chester-pdC8onszs6o-unsplash.jpg"
       size={35}
     />
 
-    <Avatar className="avatar"
+    <Avatar
+      className="avatar"
       size={35}
-      icon={
-        <IconFont
-          type="icon-tuichu"
-          size="21.25px"
-        />
-      }
-      style={{ backgroundColor: "#EE4266", marginLeft: "15px"}}
+      icon={<IconFont type="icon-tuichu" size="21.25px" />}
+      style={{ backgroundColor: "#EE4266", marginLeft: "15px" }}
     />
   </div>
 );
