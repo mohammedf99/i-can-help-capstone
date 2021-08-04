999d85d5acf82b76a5542bc92eb18b3748d91b79
Omerrj <67536912+Omerrj@users.noreply.github.com>
2021-08-04 14:21:00 +0300

    backend functions and some quick changes in other files

--- src/components/Navbar/SignedinNavbar.jsx
+++ src/components/Navbar/SignedinNavbar.jsx
@@ -1,30 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import { Avatar } from "antd";
 
 import { createFromIconfontCN } from "@ant-design/icons";
 import { signout } from "../../Utilities/FirebaseUtilities";
+import { AuthContext } from "../../Utilities/auth/Auth";
 
 const IconFont = createFromIconfontCN({
   scriptUrl: "//at.alicdn.com/t/font_8d5l8fzk5b87iudi.js",
 });
 
-const SignedinNavbar = () => (
-  <div className="btn">
-    <Avatar
-      className="avatar"
-      src="https://images.squarespace-cdn.com/content/v1/5fff77cecbbc994cdc1ee140/1610738319344-3G1CDOQBSD6BF3F5U3JD/billy-chester-pdC8onszs6o-unsplash.jpg"
-      size={35}
-    />
+const SignedinNavbar = () => {
+  const { userData } = useContext(AuthContext);
+  return (
+    <div className="btn">
+      <Avatar className="avatar" src={userData?.picture} size={35} />
 
-    <Avatar
-      className="avatar"
-      size={35}
-      icon={<IconFont type="icon-tuichu" size="21.25px" />}
-      style={{ backgroundColor: "#EE4266", marginLeft: "15px" }}
-      onClick={() => signout()}
-    />
-  </div>
-);
+      <Avatar
+        className="avatar"
+        size={35}
+        icon={<IconFont type="icon-tuichu" size="21.25px" />}
+        style={{ backgroundColor: "#EE4266", marginLeft: "15px" }}
+        onClick={() => signout()}
+      />
+    </div>
+  );
+};
 
 export default SignedinNavbar;
