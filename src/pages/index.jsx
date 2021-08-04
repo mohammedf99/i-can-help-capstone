1d8eac13dbb01d7b03de43e6020a1f349eb576c9
mohammedf99 <74563035+mohammedf99@users.noreply.github.com>
2021-08-04 13:35:12 +0300

    to view the page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,12 +4,13 @@ import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 import WelcomePage from "./welcome";
 import MyPostsPage from "./myPosts/myPostsPage";
+import PinnedPostsPage from "./pinnedPosts/pinnedPostsPage";
 
 function Homepage() {
   // This controls the direction of the page for RTL languages
   const { t } = useTranslation("common");
 
-  return <MyPostsPage />;
+  return <PinnedPostsPage />;
 }
 
 // This function below should exist in everypage
