a842803b8e83404e0fa872a396848f0c7c7bfb3c
Omerrj <67536912+Omerrj@users.noreply.github.com>
2021-08-04 12:33:18 +0300

    add components to make welcomepage (#53)

--- src/pages/welcome/welcomePage.jsx
+++ src/pages/welcome/welcomePage.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Layout from "../../components/Layout/Layout";
+import Category from "../../components/Category/Category";
+import HomeHero from "../../components/HomeHero/HomeHero";
+import StatsCounter from "../../components/StatsCounter/StatsCounter";
+import Testimonials from "../../components/Testimonials/Testimonials";
 
 function WelcomePage() {
-  return <h1>Welcome Page</h1>;
+  return (
+    <Layout>
+      <HomeHero />
+      <StatsCounter />
+      <Category />
+      <Testimonials />
+    </Layout>
+  );
 }
 
 export default WelcomePage;
