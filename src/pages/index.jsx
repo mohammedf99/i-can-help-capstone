5b83ef59d4ec723a3938940865e6270625b74c29
mohammedf99 <74563035+mohammedf99@users.noreply.github.com>
2021-08-04 17:40:16 +0300

    welcome page as index

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,25 +1,14 @@
 import React from "react";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import Navbar from "../components/Navbar/Navbar";
-import Footer from "../components/Footer/Footer";
-import UpperSection from "../components/Profile/UpperSection/UpperSection";
-import LowerSection from "../components/Profile/LowerSection/LowerSection";
-// import { Navbar } from "@components/Navbar/Navbar";
+
+import WelcomePage from "./welcome";
 
 function Homepage() {
   // This controls the direction of the page for RTL languages
   const { t } = useTranslation("common");
 
-  return (
-    <div>
-      {t("title")}
-      <Navbar />
-      {/* <UpperSection /> */}
-      <LowerSection />
-      <Footer />
-    </div>
-  );
+  return <WelcomePage />;
 }
 
 // This function below should exist in everypage
