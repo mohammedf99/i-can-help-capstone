65a33462bcaed249525a1c24d91178d1655ea611
Omerrj <67536912+Omerrj@users.noreply.github.com>
2021-08-04 14:25:13 +0300

    Backend functions and some little changes (#55)
    * add authenticated navbar with some other fixes
    
    * backend functions and some quick changes in other files

--- src/Utilities/auth/Auth.js
+++ src/Utilities/auth/Auth.js
@@ -1,18 +1,22 @@
 import React, { useEffect, useState, createContext, useContext } from "react";
 import { useRouter } from "next/router";
 import Firebase from "../../Firebase";
-import { getUser } from "../FirebaseUtilities";
+import { getPosts, getUser } from "../FirebaseUtilities";
 
 export const AuthContext = createContext({
   currentUser: false,
   loading: true,
   userData: null,
+  posts: [],
 });
 
 export const AuthProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
   const [loading, setLoading] = useState(true);
   const [userData, setUserData] = useState({});
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => getPosts((posts) => setPosts(posts)), []);
 
   const authStateChange = (user) => {
     if (user) getUser(user.uid).then(setUserData);
@@ -27,7 +31,7 @@ export const AuthProvider = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ currentUser, loading, userData }}>
+    <AuthContext.Provider value={{ currentUser, loading, userData, posts }}>
       {children}
     </AuthContext.Provider>
   );
