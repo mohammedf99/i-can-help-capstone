6deb981d2cdebb1dd2cb4720ffd7631b1cd69738
mohammedf99 <74563035+mohammedf99@users.noreply.github.com>
2021-08-04 13:34:50 +0300

    structure of the pinned page

--- src/pages/pinnedPosts/pinnedPostsPage.jsx
+++ src/pages/pinnedPosts/pinnedPostsPage.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import UpperSection from "../../components/Profile/UpperSection/UpperSection";
+import ProfilePost from "../../components/ProfilePost/ProfilePost";
+import { ProfileSection, PostsContainer } from "./pinnedPostsPage.styled";
 
 function PinnedPostsPage() {
-  return <h1>Pinned posts</h1>;
+  return (
+    <>
+      <ProfileSection>
+        <UpperSection />
+      </ProfileSection>
+
+      <PostsContainer>
+        <h3 className="my-post-title">Pinned posts</h3>
+        <div className="posts">
+          <ProfilePost />
+          <ProfilePost />
+        </div>
+      </PostsContainer>
+    </>
+  );
 }
 
 export default PinnedPostsPage;
