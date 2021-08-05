6cc9ca19c6b3a8bf8d49780c83090d2b9cb340d1
mohammedf99 <74563035+mohammedf99@users.noreply.github.com>
2021-08-05 15:02:57 +0300

    add layout to the page

--- src/pages/pinnedPosts/pinnedPostsPage.jsx
+++ src/pages/pinnedPosts/pinnedPostsPage.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import UpperSection from "../../components/Profile/UpperSection/UpperSection";
 import ProfilePost from "../../components/ProfilePost/ProfilePost";
 import { ProfileSection, PostsContainer } from "./pinnedPostsPage.styled";
+import Layout from "../../components/Layout/Layout";
 
 function PinnedPostsPage() {
   return (
-    <>
+    <Layout>
       <ProfileSection>
         <UpperSection />
       </ProfileSection>
-
       <PostsContainer>
         <h3 className="my-post-title">Pinned posts</h3>
         <div className="posts">
@@ -17,7 +17,7 @@ function PinnedPostsPage() {
           <ProfilePost />
         </div>
       </PostsContainer>
-    </>
+    </Layout>
   );
 }
 
