e8cd6f31f3651eb94d96bdcfff302dbbe21fb4ba
mohammedf99 <74563035+mohammedf99@users.noreply.github.com>
2021-08-05 14:28:00 +0300

    change back to welcome page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,13 +3,12 @@ import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 import WelcomePage from "./welcome";
-import MyPostsPage from "./myPosts/myPostsPage";
 
 function Homepage() {
   // This controls the direction of the page for RTL languages
   const { t } = useTranslation("common");
 
-  return <MyPostsPage />;
+  return <WelcomePage />;
 }
 
 // This function below should exist in everypage
