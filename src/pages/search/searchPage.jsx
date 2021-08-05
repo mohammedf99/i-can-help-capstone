d56a4fba4bf730a881ba3b0cc6547045d05ced31
mohammedf99 <74563035+mohammedf99@users.noreply.github.com>
2021-08-05 14:17:15 +0300

    add layout and titles

--- src/pages/search/searchPage.jsx
+++ src/pages/search/searchPage.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import Post from "../../components/Post/Post";
+import PostFiltering from "../../components/PostFiltering/PostFiltering";
+import { SearchContainer } from "./searchPage.styled";
+import Layout from "../../components/Layout/Layout";
 
 function SearchPage() {
-  return <h1>Search Page</h1>;
+  return (
+    <Layout>
+      <SearchContainer>
+        <div className="filter-section">
+          <h2 className="title">Filter by...</h2>
+          <PostFiltering />
+        </div>
+        <div className="post-section">
+          <h2 className="title">Search result...</h2>
+          <Post />
+          <Post />
+        </div>
+      </SearchContainer>
+    </Layout>
+  );
 }
 
 export default SearchPage;
