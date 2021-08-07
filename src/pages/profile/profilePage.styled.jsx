bd3b6a33a4cae3377ed022554771d01b966a54f8
mohammedf99 <74563035+mohammedf99@users.noreply.github.com>
2021-08-07 15:46:16 +0300

    add margin to the page

--- src/pages/profile/profilePage.styled.jsx
+++ src/pages/profile/profilePage.styled.jsx
@@ -1,18 +1,18 @@
 import styled from "styled-components";
 
 export const ProfileContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: 2rem 0;
 
-width: 100%;
-display: flex;
-flex-direction: column;
-justify-content: center;
-
-.upper-section, .lower-section {
+  .upper-section,
+  .lower-section {
     width: 100%;
     display: flex;
     justify-content: center;
     margin-top: 1rem;
     margin-bottom: 1rem;
-}
-
+  }
 `;
