a2100f439b2bf23bcdd246902df37882d78341c6
mohammedf99 <74563035+mohammedf99@users.noreply.github.com>
2021-08-07 15:49:15 +0300

    set welcome page as index

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,14 +3,12 @@ import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 import WelcomePage from "./welcome";
-import ProfilePage from "./profile/profilePage";
-
 
 function Homepage() {
   // This controls the direction of the page for RTL languages
   const { t } = useTranslation("common");
 
-  return <ProfilePage />;
+  return <WelcomePage />;
 }
 
 // This function below should exist in everypage
