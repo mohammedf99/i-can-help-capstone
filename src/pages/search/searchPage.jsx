bdda3bca5341284cf729775943a7d4f22d562c96
Omerrj <67536912+Omerrj@users.noreply.github.com>
2021-08-09 00:02:31 +0300

    add search functionality

--- src/pages/search/searchPage.jsx
+++ src/pages/search/searchPage.jsx
@@ -11,6 +11,40 @@ function SearchPage() {
   const { posts } = useContext(DataContext);
   const [filteredPosts, setFilteredPosts] = useState(posts);
   useEffect(() => setFilteredPosts(posts), [posts]);
+
+  useEffect(() => {
+    if (query.q && posts.length > 0) {
+      console.log({ posts });
+      let queryPosts = posts.map((post) => {
+        const foundPost = Object.values(post.data()).find((value) => {
+          console.log({
+            value: value
+              ?.toString()
+              .toLowerCase()
+              .includes(query.q.toLowerCase()),
+            value1: value,
+            value2: query.q.toString(),
+          });
+          return value
+            ?.toString()
+            .toLowerCase()
+            .includes(query.q.toLowerCase());
+        });
+
+        console.log({ foundPost });
+
+        if (foundPost !== undefined) return post;
+      });
+
+      //remove all undefined from array
+      queryPosts = queryPosts.filter((post) => post !== undefined);
+
+      console.log(queryPosts);
+
+      setFilteredPosts(queryPosts);
+    }
+  }, [query, posts]);
+
   return (
     <Layout>
       <SearchContainer>
@@ -25,7 +59,7 @@ function SearchPage() {
         <div className="post-section">
           <h2 className="title">Search result...</h2>
           {filteredPosts.map((post) => (
-            <Post data={{ ...post.data(), id: post.id }} />
+            <Post data={{ ...post?.data(), id: post.id }} />
           ))}
         </div>
       </SearchContainer>
