895afb5ee19cdca8eefd3080d5b14189f9593712
mohammedf99 <74563035+mohammedf99@users.noreply.github.com>
2021-08-09 15:39:31 +0300

    add translation to the search page

--- src/pages/search/searchPage.jsx
+++ src/pages/search/searchPage.jsx
@@ -1,19 +1,22 @@
 import React from "react";
+import { useTranslation, appWithTranslation, i18n } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import Post from "../../components/Post/Post";
 import PostFiltering from "../../components/PostFiltering/PostFiltering";
 import { SearchContainer } from "./searchPage.styled";
 import Layout from "../../components/Layout/Layout";
 
 function SearchPage() {
+  const { t } = useTranslation("search");
   return (
     <Layout>
       <SearchContainer>
         <div className="filter-section">
-          <h2 className="title">Filter by...</h2>
+          <h2 className="title">{t("filterBy")}...</h2>
           <PostFiltering />
         </div>
         <div className="post-section">
-          <h2 className="title">Search result...</h2>
+          <h2 className="title">{t("searchRes")}...</h2>
           <Post />
           <Post />
         </div>
@@ -23,3 +26,9 @@ function SearchPage() {
 }
 
 export default SearchPage;
+
+export const getStaticProps = async ({ locale }) => ({
+  props: {
+    ...(await serverSideTranslations(locale, ["search", "common"])),
+  },
+});
