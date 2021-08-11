4e03e686cd9738ba8ab9cad8dd06de31f6400608
Salah Al-Dhaferi <binaryminded@gmail.com>
2021-08-11 13:07:26 +0300

    added translation

--- src/components/Footer/LangDropdown.jsx
+++ src/components/Footer/LangDropdown.jsx
@@ -1,9 +1,13 @@
+import React from "react";
 import { Menu, Dropdown, Button, message, Space, Tooltip } from "antd";
 import { DownOutlined, UserOutlined } from "@ant-design/icons";
 import { useTranslation } from "next-i18next";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 const LanguageDropdown = () => {
   const { t } = useTranslation("footer");
+  const router = useRouter();
 
   function handleButtonClick(e) {
     message.info("Click on left button.");
@@ -14,8 +18,16 @@ const LanguageDropdown = () => {
 
   const menu = (
     <Menu onClick={handleMenuClick}>
-      <Menu.Item key="1">English</Menu.Item>
-      <Menu.Item key="2">Turkish</Menu.Item>
+      <Menu.Item key="1">
+        <Link href={(router.pathname, router.query)} locale="en">
+          English
+        </Link>
+      </Menu.Item>
+      <Menu.Item key="2">
+        <Link href={(router.pathname, router.query)} locale="tr">
+          Turkish
+        </Link>
+      </Menu.Item>
     </Menu>
   );
 
